a514ab23e3ebf88467ade25c70707e5dafdfb6ad
kylan02 <31520026+kylan02@users.noreply.github.com>
2024-05-20 12:52:02 -0700

    fixed margin top profile page

--- packages/react-frontend/src/profile/profile.jsx
+++ packages/react-frontend/src/profile/profile.jsx
@@ -7,24 +7,26 @@ const Profile = () => {
     return (
         <div className="container">
             <Header />
-            <Sidebar />
-            <div className="main-content">
-                <div className="profile-section">
-                    <div className="user-section">
-                        <div className="user-photo">User Photo</div>
-                        <div className="user-details">
-                            <div className="user-name">Name: [User Name]</div>
-                            <div className="books-read"># Books Read: [Number]</div>
-                            <div className="pages-read">Total Pages Read: [Number]</div>
-                            <div className="user-rank">Rank: [Rank]</div>
-                            <button className="edit-button">Edit</button>
-                            <button className="logout-button">Logout</button>
-                            <button className="delete-account-button">Delete my account</button>
+            <div className="content-wrapper">
+                <Sidebar />
+                <div className="main-content">
+                    <div className="profile-section">
+                        <div className="user-section">
+                            <div className="user-photo">User Photo</div>
+                            <div className="user-details">
+                                <div className="user-name">Name: [User Name]</div>
+                                <div className="books-read"># Books Read: [Number]</div>
+                                <div className="pages-read">Total Pages Read: [Number]</div>
+                                <div className="user-rank">Rank: [Rank]</div>
+                                <button className="edit-button">Edit</button>
+                                <button className="logout-button">Logout</button>
+                                <button className="delete-account-button">Delete my account</button>
+                            </div>
                         </div>
-                    </div>
-                    <div className="bio-section">
-                        <div className="user-bio">User Created Bio</div>
-                        <div className="top-rated-books">Top Rated Books</div>
+                        <div className="bio-section">
+                            <div className="user-bio">User Created Bio</div>
+                            <div className="top-rated-books">Top Rated Books</div>
+                        </div>
                     </div>
                 </div>
             </div>
