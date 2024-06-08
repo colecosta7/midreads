313f640fc4640fce8ad99ce6bba4809ece574bb6
Cole Costa <81593806+colecosta7@users.noreply.github.com>
2024-06-07 21:16:56 -0700

    Update user.mjs

--- packages/express-backend/models/user.mjs
+++ packages/express-backend/models/user.mjs
@@ -12,37 +12,44 @@ const UserSchema = new mongoose.Schema(
     uid: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
-    booksToRead: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Book',
-      default: function() {
-        return [];
-      }
-    }],
-    library: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Book',
-      default: function() {
-        return [];
-      }
-    }],
+    booksToRead: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+        default: function () {
+          return [];
+        },
+      },
+    ],
+    library: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+        default: function () {
+          return [];
+        },
+      },
+    ],
     bio: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
-    friends: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      default: []
-    }],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: [],
+      },
+    ],
     photo: {
-       type: String,
-       default: "https: //upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-    }
+      type: String,
+      default:
+        "https: //upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+    },
   },
-  { collection: "users_list" }
+  { collection: "users_list_new" }
 );
 
 const User = mongoose.model("User", UserSchema);
