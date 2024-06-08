da52b6a2bd6ee186882430a8f3ef75ed35caa88f
Cole Costa <81593806+colecosta7@users.noreply.github.com>
2024-06-07 21:18:31 -0700

    Update book-services.mjs

--- packages/express-backend/services/book-services.mjs
+++ packages/express-backend/services/book-services.mjs
@@ -6,64 +6,78 @@ dotenv.config();
 
 mongoose.set("debug", true);
 
-mongoose.connect(process.env.MONGODB_URI, {
-}).then(() => console.log('Connected to MongoDB Atlas'))
-.catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error("Error connecting to MongoDB Atlas:", error));
 
 async function addBook(book) {
   const bookToAdd = new bookModel(book);
-  const title =  bookToAdd.title;
+  const title = bookToAdd.title;
   const author = bookToAdd.author;
-  const badWordsSubstrings = ["fuck", "shit", "bitch", "imshrekwesandimgettingreallyrich"];
+  const badWordsSubstrings = [
+    "fuck",
+    "shit",
+    "bitch",
+    "imshrekwesandimgettingreallyrich",
+  ];
 
   function containsAnySubstring(mainString, searchStrings) {
     const lowerMainString = mainString.toLowerCase();
 
     for (const searchString of searchStrings) {
-        const lowerSearchString = searchString.toLowerCase();
+      const lowerSearchString = searchString.toLowerCase();
 
-        if (lowerMainString.includes(lowerSearchString)) {
-            return true;
-        }
+      if (lowerMainString.includes(lowerSearchString)) {
+        return true;
+      }
     }
-    return false; 
+    return false;
   }
 
-  if (containsAnySubstring(title, badWordsSubstrings) || containsAnySubstring(author, badWordsSubstrings)) {
-      return undefined;
+  if (
+    containsAnySubstring(title, badWordsSubstrings) ||
+    containsAnySubstring(author, badWordsSubstrings)
+  ) {
+    return undefined;
   } else {
     const exactMatchTitleRegex = `^${title}$`;
     const exactMatchAuthorRegex = `^${author}$`;
 
     const result = await bookModel.find({
       $and: [
-          { title: { $regex: exactMatchTitleRegex, $options: 'i' } },
-          { author: { $regex: exactMatchAuthorRegex, $options: 'i' } }
-      ]
+        { title: { $regex: exactMatchTitleRegex, $options: "i" } },
+        { author: { $regex: exactMatchAuthorRegex, $options: "i" } },
+      ],
     });
 
     if (result.length === 0) {
-        const savedBook = await bookToAdd.save();
-        return savedBook;
+      const savedBook = await bookToAdd.save();
+      return savedBook;
     } else {
-        return undefined;
+      return undefined;
     }
   }
 }
-  
-function findBooksWithSubstring(substring, startIndex, endIndex) { //will find all books containing substring, use for search
-  const promise = bookModel.find({ title: { $regex: substring, $options: 'i' } }).skip(startIndex)
-  .limit(endIndex - startIndex);
+
+function findBooksWithSubstring(substring, startIndex, endIndex) {
+  //will find all books containing substring, use for search
+  const promise = bookModel
+    .find({ title: { $regex: substring, $options: "i" } })
+    .skip(startIndex)
+    .limit(endIndex - startIndex);
   return promise;
 }
 
 function findCountOfBooksWithSubstring(substring) {
-  const promise = bookModel.countDocuments({ title: { $regex: substring, $options: 'i' } })
+  const promise = bookModel.countDocuments({
+    title: { $regex: substring, $options: "i" },
+  });
   return promise;
 }
 
 export default {
   addBook,
   findBooksWithSubstring,
-  findCountOfBooksWithSubstring
+  findCountOfBooksWithSubstring,
 };
