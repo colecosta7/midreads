6bd652fb522ca153b292a3d04bb80ad83edb71b0
Cole Costa <81593806+colecosta7@users.noreply.github.com>
2024-06-07 21:40:16 -0700

    Update header.jsx

--- packages/react-frontend/src/friends/header.jsx
+++ packages/react-frontend/src/friends/header.jsx
@@ -1,51 +1,61 @@
-import React, { useState } from 'react';
-import { useAuth } from '../Auth';
-
+import React, { useState } from "react";
+import { useAuth } from "../Auth";
 
 const Header = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [addFriendMessage, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const { currentUser } = useAuth();
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const { currentUser } = useAuth();
+  function addFriend() {
+    const friendData = {
+      friend: searchTerm,
+      user: currentUser.uid,
+    };
 
-    function addFriend() {
-        const friendData = {
-            friend: searchTerm,
-            user: currentUser.uid
-        };
-        
-        console.log(friendData);
-        const url = new URL("http://ec2-3-142-68-171.us-east-2.compute.amazonaws.com:8000/addFriend");
-   
-        const promise = fetch(url, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(friendData)
-            });
+    console.log(friendData);
+    const url = new URL("http://ec2-3-142-68-171.us-east-2.compute.amazonaws.com:8000/addFriend");
 
-        promise.then(result => {
-            if(result.status === 406) {
-                alert("Error adding friend");
-            } else {
-                alert("Friend successully added");
-            }
-        })
-    }
+    const promise = fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(friendData),
+    });
 
-    return (
-        <div className="header">
-            <div className="logo">MidReads</div>
-            <input
-                type="text"
-                placeholder="Search friends..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                style={{ color: "black" }}
-            />
-            <button onClick={addFriend}>Add friend</button>
+    promise.then((result) => {
+      if (result.status === 406) {
+        setMessage("Error adding friend.");
+        setShowMessage(true);
+      } else {
+        setMessage("Friend successfully added.");
+        setShowMessage(true);
+      }
+    });
+  }
+
+  return (
+    <div className="header">
+      <div className="logo">MidReads</div>
+      <input
+        type="text"
+        placeholder="Search friends..."
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+          setShowMessage(false);
+        }}
+        style={{ color: "black" }}
+      />
+      <button onClick={addFriend}>Add friend</button>
+      {showMessage && (
+        <div style={{ color: "maroon", paddingLeft: "15px" }}>
+          {addFriendMessage}
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Header;
