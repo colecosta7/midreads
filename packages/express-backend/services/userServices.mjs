60fae69a12729081ad8bde7f2f9c6b4ec855ea09
Cole Costa <81593806+colecosta7@users.noreply.github.com>
2024-06-07 21:19:18 -0700

    Update userServices.mjs

--- packages/express-backend/services/userServices.mjs
+++ packages/express-backend/services/userServices.mjs
@@ -7,10 +7,10 @@ dotenv.config();
 
 mongoose.set("debug", true);
 
-mongoose.connect(process.env.MONGODB_URI, {
-}).then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
-
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error("Error connecting to MongoDB Atlas:", error));
 
 function addUser(user) {
   const userToAdd = new userModel(user);
@@ -19,155 +19,123 @@ function addUser(user) {
 }
 
 function getUser(uid) {
-  return userModel.findOne({ uid: uid })
+  return userModel.findOne({ uid: uid });
 }
 
 async function updateReadLater(uid, book) {
-  let user = await userModel.findOne({ uid: uid })
+  let user = await userModel.findOne({ uid: uid });
   console.log(user);
-  if(user.booksToRead.includes(book._id)) {
+  if (user.booksToRead.includes(book._id)) {
     return undefined;
   } else {
     return userModel.updateOne(
       { uid: uid },
       { $push: { booksToRead: book._id } }
     );
-  }   
+  }
 }
 
 async function removeReadLater(uid, book) {
-  let user = await userModel.findOne({ uid: uid })
+  let user = await userModel.findOne({ uid: uid });
   console.log(user);
-  if(user.booksToRead.includes(book._id)) {
+  if (user.booksToRead.includes(book._id)) {
     return userModel.updateOne(
       { uid: uid },
       { $pull: { booksToRead: book._id } }
     );
   } else {
-    return undefined
-  }   
+    return undefined;
+  }
 }
 
 async function updateLibrary(uid, book) {
-  let user = await userModel.findOne({ uid: uid })
-  if(user.library.includes(book._id)) {
+  let user = await userModel.findOne({ uid: uid });
+  if (user.library.includes(book._id)) {
     return undefined;
   } else {
-    return userModel.updateOne(
-      { uid: uid },
-      { $push: { library: book._id } }
-    );
-  }   
+    return userModel.updateOne({ uid: uid }, { $push: { library: book._id } });
+  }
 }
 
 async function updatePhoto(uid, url) {
-  console.log("USERPHOTOURL:", url)
-  return userModel.updateOne(
-    { uid: uid },
-    { $set: {photo: url} } 
-  )
-}  
+  console.log("USERPHOTOURL:", url);
+  return userModel.updateOne({ uid: uid }, { $set: { photo: url } });
+}
 
 async function updateBio(uid, bio) {
-  return userModel.updateOne(
-    { uid: uid },
-    { $set: {bio: bio} } 
-  )
-} 
+  return userModel.updateOne({ uid: uid }, { $set: { bio: bio } });
+}
 
 async function getUserBio(uid) {
   let user = await userModel.findOne({ uid: uid });
-  if(!user){
-    console.log("user not found");
-    return null;
-  }
-  let bio = user.bio
-  return bio
-}
 
+  let bio = user.bio;
+  return bio;
+}
 
 async function getUserLibrary(uid) {
   let user = await userModel.findOne({ uid: uid });
-  if(user && user.library){
-    let library = await Promise.all(user.library.map(bookId => bookModel.findById(bookId)));
-    return library;
-  }
-  else{
-    console.log("user not found");
-    return null;
-  }
+
+  let library = await Promise.all(
+    user.library.map((bookId) => bookModel.findById(bookId))
+  );
+  return library;
 }
 
 async function getUserReadLater(uid) {
   let user = await userModel.findOne({ uid: uid });
 
-  if(!user){
-    console.log("user not found");
-    return null;
-  }
-  let readLater = await Promise.all(user.booksToRead.map(bookId => bookModel.findById(bookId)));
-  
+  let readLater = await Promise.all(
+    user.booksToRead.map((bookId) => bookModel.findById(bookId))
+  );
+
   return readLater;
 }
 
 async function getCountLibrary(uid) {
-  let user = await userModel.findOne({ uid: uid});
-  if(user && user.library){
-    let count = user.library.length;
-    return count;
-  }
-  else{
-    console.log("user not found");
-    return 0;
-  }
+  let user = await userModel.findOne({ uid: uid });
+  let count = user.library.length;
+  return count;
 }
 
 async function getCountTotalPages(uid) {
   //let user = await userModel.findOne({ uid: uid});
-  let books = await getUserLibrary(uid)
-  if (!books){
-    console.log("books not found");
-    return 0;
-  }
+  let books = await getUserLibrary(uid);
+  console.log(books);
   let pages = books.reduce((sum, book) => sum + book.numPages, 0);
-  return pages
-
+  return pages;
 }
 
 async function updateFriends(uid, friend) {
-  let user = await userModel.findOne({ uid: uid })
-  let friendObj = await userModel.findOne({userName: friend});
-  if(friendObj === null) {
+  let user = await userModel.findOne({ uid: uid });
+  let friendObj = await userModel.findOne({ userName: friend });
+  if (friendObj === null) {
     return undefined;
   }
-  if(user.friends.includes(friendObj._id)) {
+  if (user.friends.includes(friendObj._id)) {
     return undefined;
   } else {
-    return userModel.updateOne(
-      { uid: uid },
-      { $push: { friends: friendObj } }
-    );
-  }   
+    return userModel.updateOne({ uid: uid }, { $push: { friends: friendObj } });
+  }
 }
 
 async function getFriends(friendIds) {
   return userModel.find({ _id: { $in: friendIds } });
 }
 
-
 export default {
-    addUser,
-    getUser,
-    updateReadLater,
-    updateLibrary,
-    getUserLibrary,
-    getCountLibrary,
-    getCountTotalPages,
-    updateFriends,
-    getFriends,
-    getUserReadLater,
-    removeReadLater,
-    updatePhoto,
-    updateBio,
-    getUserBio
+  addUser,
+  getUser,
+  updateReadLater,
+  updateLibrary,
+  getUserLibrary,
+  getCountLibrary,
+  getCountTotalPages,
+  updateFriends,
+  getFriends,
+  getUserReadLater,
+  removeReadLater,
+  updatePhoto,
+  updateBio,
+  getUserBio,
 };
